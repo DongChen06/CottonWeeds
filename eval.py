6aa63106e79e0448ca36cb7a3f2103674b0086b7
Derekabc <chendondon19930606@gmail.com>
2021-08-04 20:34:39 -0400

    Initial commit

---
+++ eval.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch
+import torchvision
+from torchvision import datasets, models, transforms
+import torch.utils.data as data
+import multiprocessing
+from sklearn.metrics import confusion_matrix
+
+# Paths for image directory and model
+EVAL_DIR = 'data/test'
+# Load a pretrained model - resnet18, resnet50, resnet101, alexnet, squeezenet, vgg11, vgg16, densenet121, densenet161, inception
+# googlenet
+name = 'inception'
+EVAL_MODEL = name + ".pth"
+img_size = 512
+# Load the model for evaluation
+model = torch.load(EVAL_MODEL)
+model.eval()
+
+# Configure batch size and number of cpu's
+num_cpu = multiprocessing.cpu_count()
+bs = 3
+
+# Prepare the eval data loader
+eval_transform=transforms.Compose([
+        transforms.Resize(size=img_size),
+        transforms.CenterCrop(size=img_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])])
+
+eval_dataset=datasets.ImageFolder(root=EVAL_DIR, transform=eval_transform)
+eval_loader=data.DataLoader(eval_dataset, batch_size=bs, shuffle=True,
+                            num_workers=num_cpu, pin_memory=True)
+
+# Enable gpu mode, if cuda available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Number of classes and dataset-size
+num_classes = len(eval_dataset.classes)
+dsize = len(eval_dataset)
+
+# Class label names
+class_names = ['Carpetweed', 'CommonRagweed', 'Crabgrass', 'Morningglory', 'PalmerAmaranth']
+
+# Initialize the prediction and label lists
+predlist = torch.zeros(0,dtype=torch.long, device='cpu')
+lbllist = torch.zeros(0,dtype=torch.long, device='cpu')
+
+# Evaluate the model accuracy on the dataset
+correct = 0
+total = 0
+with torch.no_grad():
+    for images, labels in eval_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+        predlist = torch.cat([predlist,predicted.view(-1).cpu()])
+        lbllist = torch.cat([lbllist,labels.view(-1).cpu()])
+
+# Overall accuracy
+overall_accuracy = 100 * correct / total
+print('Accuracy of the network on the {:d} test images: {:.2f}%'.format(dsize, 
+    overall_accuracy))
+
+# Confusion matrix
+conf_mat = confusion_matrix(lbllist.numpy(), predlist.numpy())
+print('Confusion Matrix')
+print('-'*16)
+print(conf_mat,'\n')
+
+# Per-class accuracy
+class_accuracy = 100*conf_mat.diagonal()/conf_mat.sum(1)
+print('Per class accuracy')
+print('-'*18)
+for label, accuracy in zip(eval_dataset.classes, class_accuracy):
+     class_name=class_names[int(label)]
+     print('Accuracy of class %8s : %0.2f %%'%(class_name, accuracy))
+
+'''
+Sample run: python eval.py eval_ds
+'''
---
+++ train.py
@@ -0,0 +1,248 @@
+from torchvision import datasets, models, transforms
+import torch.utils.data as data
+from torch.utils.tensorboard import SummaryWriter
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from nets import *
+import time, copy
+import multiprocessing
+from torchsummary import summary
+
+# Construct argument parser
+# Set training mode: finetune, transfer, scratch
+train_mode = 'finetune'
+# Load a pretrained model - resnet18, resnet50, resnet101, alexnet, squeezenet, vgg11, vgg16, densenet121, densenet161, inception
+# googlenet
+name = 'inception'
+# Set the train and validation directory paths
+train_directory = 'data/train'
+valid_directory = 'data/val'
+# Set the model save path
+PATH = name + ".pth"
+
+# Batch size
+bs = 12
+# Number of epochs
+num_epochs = 30
+# Number of classes
+num_classes = 5
+
+# resized image: 256, 512, 1080
+img_size = 512
+
+# Number of workers
+num_cpu = multiprocessing.cpu_count()
+
+# Applying transforms to the data
+image_transforms = { 
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(size=img_size, scale=(0.8, 1.0)),
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(size=img_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(size=img_size),
+        transforms.CenterCrop(size=img_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+}
+ 
+# Load data from folders
+dataset = {
+    'train': datasets.ImageFolder(root=train_directory, transform=image_transforms['train']),
+    'valid': datasets.ImageFolder(root=valid_directory, transform=image_transforms['valid'])
+}
+ 
+# Size of train and validation data
+dataset_sizes = {
+    'train': len(dataset['train']),
+    'valid': len(dataset['valid'])
+}
+
+# Create iterators for data loading
+dataloaders = {
+    'train': data.DataLoader(dataset['train'], batch_size=bs, shuffle=True,
+                            num_workers=num_cpu, pin_memory=True, drop_last=True),
+    'valid': data.DataLoader(dataset['valid'], batch_size=bs, shuffle=True,
+                            num_workers=num_cpu, pin_memory=True, drop_last=True)
+}
+
+# Class names or target labels
+class_names = dataset['train'].classes
+print("Classes:", class_names)
+ 
+# Print the train and validation data sizes
+print("Training-set size:", dataset_sizes['train'],
+      "\nValidation-set size:", dataset_sizes['valid'])
+
+# Set default device as gpu, if available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+print("\nLoading pretrained-model for finetuning ...\n")
+
+if name == 'resnet18':
+    # Modify fc layers to match num_classes
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
+elif name == 'resnet50':
+    # Modify fc layers to match num_classes
+    model_ft = models.resnet50(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
+elif name == 'resnet101':
+    # Modify fc layers to match num_classes
+    model_ft = models.resnet101(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
+elif name == 'alexnet':
+    model_ft = models.alexnet(pretrained=True)
+    model_ft.classifier[6] = nn.Linear(4096, num_classes)
+elif name == 'vgg11':
+    model_ft = models.vgg11(pretrained=True)
+    model_ft.classifier[6] = nn.Linear(4096, num_classes)
+elif name == 'vgg16':
+    model_ft = models.vgg16(pretrained=True)
+    model_ft.classifier[6] = nn.Linear(4096, num_classes)
+elif name == 'squeezenet':
+    model_ft = models.squeezenet1_0(pretrained=True)
+    model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+elif name == 'densenet121':
+    model_ft = models.densenet121(pretrained=True)
+    num_ftrs = model_ft.classifier.in_features
+    model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+elif name == 'densenet161':
+    model_ft = models.densenet161(pretrained=True)
+    num_ftrs = model_ft.classifier.in_features
+elif name == 'inception':
+    model_ft = models.inception_v3(pretrained=True)
+    model_ft.aux_logits = False
+    # Handle the auxilary net
+    num_ftrs = model_ft.AuxLogits.fc.in_features
+    model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+    # Handle the primary net
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
+else:
+    print("Invalid model name, exiting...")
+    exit()
+
+# Transfer the model to GPU
+model_ft = model_ft.to(device)
+
+# Print model summary
+print('Model Summary:-\n')
+for num, (name, param) in enumerate(model_ft.named_parameters()):
+    print(num, name, param.requires_grad )
+if name == 'inception':
+    summary(model_ft, input_size=(3, 299, 299))
+elif name == 'densenet121' or 'densenet161':
+    pass
+else:
+    summary(model_ft, input_size=(3, img_size, img_size))
+print(model_ft)
+
+# Loss function
+criterion = nn.CrossEntropyLoss()
+
+# Optimizer 
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Learning rate decay
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+# Model training routine 
+print("\nTraining:-\n")
+def train_model(model, criterion, optimizer, scheduler, num_epochs=30):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    # Tensorboard summary
+    writer = SummaryWriter(log_dir='runs/' + name)
+    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device, non_blocking=True)
+                labels = labels.to(device, non_blocking=True)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # Record training loss and accuracy for each phase
+            if phase == 'train':
+                writer.add_scalar('Train/Loss', epoch_loss, epoch)
+                writer.add_scalar('Train/Accuracy', epoch_acc, epoch)
+                writer.flush()
+            else:
+                writer.add_scalar('Valid/Loss', epoch_loss, epoch)
+                writer.add_scalar('Valid/Accuracy', epoch_acc, epoch)
+                writer.flush()
+
+            # deep copy the model
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+# Train the model
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=num_epochs)
+# Save the entire model
+print("\nSaving the model...")
+torch.save(model_ft, PATH)
